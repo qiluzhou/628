bf2a71f69239a86520b3c573616fbb5bf13ac7f2
Qiluzhou <66503559+qiluzhou@users.noreply.github.com>
2021-10-18 21:19:47 -0500

    Add files via upload

--- bodyfat.R
+++ bodyfat.R
@@ -31,80 +31,71 @@ data[,5]=data[,5]*0.45359237
 data[,6] = data[,6]*2.54
 
 #Model 1&2: only fit height or weight
-plot(data$DENSITY~data$HEIGHT)
-fit1=lm(data$DENSITY~exp(data$HEIGHT))
+plot(data$BODYFAT~data$HEIGHT)
+fit1=lm(data$BODYFAT~exp(data$HEIGHT))
 summary(fit1)
 
-plot(data$DENSITY~data$WEIGHT)
-fit2=lm(data$DENSITY~data$WEIGHT)
+plot(data$BODYFAT~data$WEIGHT)
+fit2=lm(data$BODYFAT~data$WEIGHT)
 summary(fit2)
 
 #Model3: fit both height and weight R^2=0.49
-fit3=lm(data$DENSITY~data$WEIGHT+data$HEIGHT)
+fit3=lm(data$BODYFAT~data$WEIGHT+data$HEIGHT)
 summary(fit3)
 plot(fit3$residuals~fit3$fitted.values)
 
 #model4: fit height, weight and age R^2=0.52
-fit4=lm(data$DENSITY~data$WEIGHT+data$HEIGHT+data$AGE)
+fit4=lm(data$BODYFAT~data$WEIGHT+data$HEIGHT+data$AGE)
 summary(fit4)
 
 #model5: stepwise regression
-library(caret)
-ctrl<-trainControl(method = "cv",number = 10)#设置10倍交叉验证
 
-step_model<-train(DENSITY~HEIGHT+WEIGHT+AGE+NECK+CHEST+ABDOMEN+HIP+THIGH+KNEE+ANKLE+BICEPS+FOREARM+WRIST,
-                  method = "leapSeq",
-                  tuneGrid = data.frame(nvmax = 1:5),#变量个数
-                  trControl = ctrl,
+
+
+step_model<-lm(BODYFAT~HEIGHT+WEIGHT+AGE+NECK+CHEST+ABDOMEN+HIP+THIGH+KNEE+ANKLE+BICEPS+FOREARM+WRIST,
                   data=data
 )
-step_model$results
-
-step_model$finalModel
-coef(step_model$finalModel,4)
-# best model weight, abdomen, forearm, wrist R^2 0.708
-
-coef(step_model$finalModel,2)# most imp:abdomen
+t_model=step(step_model)
+summary(t_model)
+mean(t_model$residuals^2)
 
 #for simplicity, use abdomen and weight
-fit5=lm(data$DENSITY~data$WEIGHT+data$ABDOMEN)
+fit5=lm(data$BODYFAT~data$WEIGHT+data$ABDOMEN)
 par(mfrow=c(2,2))
 summary(fit5)
 plot(fit5)
 mean(fit5$residuals^2)
 
 #model6(consider interaction): full model
-fit6=lm(data$DENSITY~data$WEIGHT*data$ABDOMEN*data$AGE*data$HEIGHT)
+fit6=lm(data$BODYFAT~data$AGE*data$ABDOMEN*data$HEIGHT)
 summary(fit6)
-plot(fit6)
 
 
 #lasso regression
 library(glmnet)
-x = model.matrix(DENSITY~.-IDNO-DENSITY,data)[,-1]
-y = data$DENSITY
+x = model.matrix(BODYFAT~.-IDNO-DENSITY,data)[,-1]
+y = data$BODYFAT
 set.seed(123) 
 cv.lasso <- cv.glmnet(x, y, alpha = 1, family = "gaussian")
 model <- glmnet(x, y, alpha = 1, family = "gaussian",
                 lambda = cv.lasso$lambda.min)
 coef(model)
-fit_lasso = lm(DENSITY~AGE+HEIGHT+NECK+ABDOMEN+FOREARM+WRIST,data=data)
+fit_lasso = lm(BODYFAT~AGE+HEIGHT+NECK+ABDOMEN+FOREARM+WRIST,data=data)
 summary(fit_lasso)
 mean(fit_lasso$residuals^2)
 
 
 #model7: use only significant two-factor interaction
 #use this one as final model
-fit7=lm(data$DENSITY~data$WEIGHT+data$ABDOMEN+data$HEIGHT
-        +data$WEIGHT:data$ABDOMEN+data$WEIGHT:data$HEIGHT)
+fit7=lm(data$BODYFAT~ABDOMEN*WEIGHT, data=data)
 summary(fit7)
 
 #MSE
 mean(fit7$residuals^2)
 plot(fit7)
 confint(fit7)
-#predict michael phelps's density
-ph=c(1,86,83.38, 193,7138,16791)
+#predict michael phelps's bodyfat
+ph=c(1,83.38, 86,7170.68)
 sum(fit7$coefficients*ph)
 
 #model diagnostics
